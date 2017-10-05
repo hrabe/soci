e9cebac7b6445c6746d5f93fafc5880ad9f746a1
Heiko Rabe <heiko.rabe@invision.de>
2017-10-05 11:57:56 +0200

    Ubuntu install

--- scripts/travis/before_install_odbc.sh
+++ scripts/travis/before_install_odbc.sh
@@ -8,38 +8,43 @@ source ${TRAVIS_BUILD_DIR}/scripts/travis/common.sh
 sudo apt-get install -qq \
     tar bzip2 \
     unixodbc-dev \
-    libmyodbc odbc-postgresql
+    libmyodbc odbc-postgresql libsqliteodbc 
 
 sudo odbcinst -i -d -f /usr/share/libmyodbc/odbcinst.ini
-
-echo 'install sqlite-dev'
-sudo apt-get install libsqlite-dev
-
-echo 'install fakeroot'
-sudo apt-get install fakeroot
-
-# Install sqlite3odbc driver
-echo 'build sqlite3 odbc driver'
-wget 'http://www.ch-werner.de/sqliteodbc/sqliteodbc-0.9995.tar.gz'
-tar xzf sqliteodbc-*.tar.gz
-cd sqliteodbc-*
-sudo ./configure --build=x86_64-linux-gnu && make
-sudo make install
-
-echo 'install sqlite3 odbc driver'
-sudo odbcinst -i -d -f ./debian/unixodbc.ini
-
-echo 'debian path'
-ls -Al /usr/lib/odbc/
-
-echo 'build path'
-ls -Al /usr/local/lib/
 
 echo 'show odbcinst.ini'
 cat /etc/odbcinst.ini
 
-# Ubuntu 12.04 is architecture dependend
-# /usr/lib/x86_64-linux-gnu/odbc/libmyodbc.so
-# /usr/lib/i386-linux-gnu/odbc/libmyodbc.so
-echo 'driver path Ubuntu'
-ls -Al /usr/lib/x86_64-linux-gnu/odbc/
+# sudo apt-get install libsqliteodbc unixodbc
+
+# echo 'install sqlite-dev'
+# sudo apt-get install libsqlite-dev
+#
+# echo 'install fakeroot'
+# sudo apt-get install fakeroot
+#
+# # Install sqlite3odbc driver
+# echo 'build sqlite3 odbc driver'
+# wget 'http://www.ch-werner.de/sqliteodbc/sqliteodbc-0.9995.tar.gz'
+# tar xzf sqliteodbc-*.tar.gz
+# cd sqliteodbc-*
+# sudo ./configure --build=x86_64-linux-gnu && make
+# sudo make install
+#
+# echo 'install sqlite3 odbc driver'
+# sudo odbcinst -i -d -f ./debian/unixodbc.ini
+#
+# echo 'debian path'
+# ls -Al /usr/lib/odbc/
+#
+# echo 'build path'
+# ls -Al /usr/local/lib/
+#
+# echo 'show odbcinst.ini'
+# cat /etc/odbcinst.ini
+#
+# # Ubuntu 12.04 is architecture dependend
+# # /usr/lib/x86_64-linux-gnu/odbc/libmyodbc.so
+# # /usr/lib/i386-linux-gnu/odbc/libmyodbc.so
+# echo 'driver path Ubuntu'
+# ls -Al /usr/lib/x86_64-linux-gnu/odbc/

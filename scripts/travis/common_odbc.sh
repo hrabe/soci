b88b9a9d8147479220d6cae741bec94604cd5ac9
Heiko Rabe <heiko.rabe@invision.de>
2017-10-11 15:03:04 +0200

    Firebird trial final

--- scripts/travis/common_odbc.sh
+++ scripts/travis/common_odbc.sh
@@ -86,14 +86,9 @@ download_and_install_firebird_driver_devart_trial()
 download_and_install_firebird_driver_devart_trial2()
 {  
   mkdir -p /tmp/firebird-devart
-  # pushd /tmp/firebird-devart
-  wget 'https://www.devart.com/odbc/firebird/devartodbcfirebird-linux.tar' --progress=bar:force
-  tar -xf devartodbcfirebird-linux.tar -C /tmp/firebird-devart/
+  wget 'https://www.devart.com/odbc/firebird/devartodbcfirebird-linux.tar' -P /tmp/firebird-devart/ --progress=bar:force
+  tar -xf /tmp/firebird-devart/devartodbcfirebird-linux.tar -C /tmp/firebird-devart/
   library=$(ls /tmp/firebird-devart | grep .x64.so)
-  # popd
-  # pathodbc=$DEFIODBCINIUNIX
-  # lic="TRIAL"
-  echo $library
   sudo ln -s -f "/tmp/firebird-devart/"$library "/usr/lib/x86_64-linux-gnu/odbc/"$library
   sudo "/tmp/firebird-devart/firebirdodbcsetup_x64" "TRIAL" "/etc/" "/usr/lib/x86_64-linux-gnu/odbc/"$library x64
 }

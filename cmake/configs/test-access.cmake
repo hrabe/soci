3069877695423cb6208d8d6299b80fed94381b7a
Heiko Rabe <heiko.rabe@invision.de>
2017-10-11 20:29:35 +0200

    Access 2

--- cmake/configs/test-access.cmake
+++ cmake/configs/test-access.cmake
@@ -9,5 +9,5 @@ MaxScanRows=8
 MaxBufferSize=2048
 FIL=MS Access
 DriverId=25
-DefaultDir=@TEST_ACCESS_PATH@\tests\odbc
-DBQ=@TEST_ACCESS_PATH@\tests\odbc\soci_test.mdb
+DefaultDir=@TEST_ACCESS_PATH@/tests/odbc
+DBQ=@TEST_ACCESS_PATH@/tests/odbc/soci_test.mdb
